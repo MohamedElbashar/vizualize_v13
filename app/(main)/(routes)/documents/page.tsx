902fd4a878726b95bf1ec2c2edd55e9c30e5ef25
Mohamed Elbashar <mohamedbashar200@gmail.com>
2024-01-03 01:37:52 +0200

    FEAT : add create endpoint and integrate it with the front-end

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -3,16 +3,25 @@ import Image from "next/image";
 import { useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { insertOne } from "../../_action/insertOne";
 
 export default function DocumentPage() {
   const { user } = useUser();
+  console.log(user?.id);
+  //TODO: ERROR PAGE
+  if (!user) return null;
+
+  const onClickHandler = async () => {
+    const documents = await insertOne({ title: "untitled", userId: user.id });
+    console.log(documents);
+  };
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image src="/books.png" height="300" width="300" alt="Empty" />
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Notes
       </h2>
-      <Button>
+      <Button onClick={onClickHandler}>
         <PlusCircle className="h-4 w-4 mr-2" />
         New Note
       </Button>
---
+++ app/(main)/_action/insertOne.tsx
@@ -0,0 +1,36 @@
+"use server";
+
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+interface InsertOneParams {
+  title: string;
+  userId: string;
+  parentDocument?: number;
+}
+
+export const insertOne = async ({
+  title,
+  userId,
+  parentDocument,
+}: InsertOneParams): Promise<IDocumentType | null> => {
+  try {
+    const supabase = createServerActionClient<Database>({ cookies });
+    console.log("createDocument", title, userId, parentDocument);
+
+    const { data, error } = await supabase.from("documents").insert({
+      title,
+      parentDocument,
+      userId,
+      isPublished: false,
+      isArchived: false,
+    });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+    return data;
+  } catch (err) {
+    throw new Error(String(err));
+  }
+};
