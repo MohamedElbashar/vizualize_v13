4bf2ad7e023379c27e4d7957b30706079f121b1e
Mohamed Elbashar <mohamedbashar200@gmail.com>
2024-01-03 20:35:20 +0200

    FIX : solve collapse div

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -143,6 +143,7 @@ export const Navigation = () => {
       >
         <div>
           <div
+            onClick={collapse}
             role="button"
             className={cn(
               "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
